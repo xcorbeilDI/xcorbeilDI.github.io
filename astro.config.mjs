3b671595ad25db35e58539d8f5f165a29b92a933
Xavier Corbeil <xcorbeil@dialoginsight.com>
2026-03-23 14:43:51 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
 const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
 const owner = process.env.GITHUB_REPOSITORY_OWNER;
 
-const base = isGitHubActions && repoName ? `/${repoName}/` : "/";
+const isUserOrOrgPagesRepo = repoName ? repoName.endsWith(".github.io") : false;
+const base =
+  isGitHubActions && repoName && !isUserOrOrgPagesRepo ? `/${repoName}/` : "/";
 const site = owner ? `https://${owner}.github.io` : undefined;
 
 export default defineConfig({
